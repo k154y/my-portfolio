99aca75910cc1852d325f962807af42aa8ed9978
k154y <yveskwizera154@gmail.com>
2026-02-03 17:32:41 +0200

    first commit

---
+++ src/hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero(){
+    return (
+        <>
+        <div className="flex flex-col justify-between items-center p-10">
+            <img src="/profile.jpeg" alt="profile" className="rounded-full w-40 h-40 object-cover" />
+            <h1 className="text-4xl md:text-6xl font-bold dark:text-white mt-6">Hi, I’m Kwizera Yves </h1>
+            <h3 className="text-xl md:text-2xl text-gray-700 dark:text-gray-300 mt-2">Full-Stack Web Developer & Cybersecurity Enthusiast</h3>
+            <p className="text-base md:text-lg text-gray-600 dark:text-gray-300 text-center max-w-2xl mt-4">
+               I build secure, scalable, and modern web applications using React, Node.js, Express, and Tailwind CSS.
+            </p>
+            <div className="flex flex-row justify-center md:justify-between items-center gap-6 mt-6">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded">View Projects</button>
+                <button className="px-4 py-2 border border-gray-300 rounded">Contact Me</button>
+            </div>
+        </div>
+        </>
+    );
+}
---
+++ src/navbar.jsx
@@ -0,0 +1,24 @@
+export default function Navbar(){
+    return (
+        <>
+        <nav className="sticky top-0 z-50 flex justify-between items-center bg-blue-50 text-gray-900 dark:bg-slate-700 dark:text-white px-6 py-4 font-semibold shadow-md">
+            <h2 className="text-xl">
+                Portfolio
+            </h2>
+            <div>
+                  <ul className="flex gap-6">
+                    
+                        <li><a href="#home" className="hover:text-blue-600 transition">Home</a></li>
+                        <li><a href="#about" className="hover:text-blue-600 transition">About</a></li>
+                        <li><a href="#skills" className="hover:text-blue-600 transition">Skills</a></li>
+                        <li><a href="#projects" className="hover:text-blue-600 transition">Projects</a></li>
+                        <li><a href="#ach" className="hover:text-blue-600 transition">Achievements</a></li>
+                        <li><a href="#contact" className="hover:text-blue-600 transition">Contact</a></li>
+                    </ul>
+            </div>
+        </nav>
+        
+        
+        </>
+    );
+}
